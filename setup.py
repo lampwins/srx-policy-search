07c5554ecb105ad7d3e166fc920a7cce8a052250
John Anderson <lampwins@gmail.com>
2016-05-23 16:39:53 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='srx-policy-search',
+    version='0.0.1',
+    packages=['paramiko'],
+    url='https://github.com/lampwins/srx-policy-search',
+    license='MIT',
+    author='John Anderson',
+    author_email='lampwins@gmail.com',
+    description='Search security policy on the Juniper SRX platform'
+)
